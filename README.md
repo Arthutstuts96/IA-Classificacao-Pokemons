0b096b8143a65c1a6abf31ea8bc2504fee63cddf
Arthutstuts96 <oarthurlima1575@gmail.com>
2025-11-09 13:41:35 -0300

    Rede neural MLP e primeiros resultados

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # IA-Classificacao-Pokemons
 
+O objetivo desta atividade foi criar uma rede neural MLP capaz de resolver um problema de classificação definido, particularmente o de adivinhar o tipo de um dado pokemon a partir de alguns parâmetros coletados de uma base de dados. Caso o pokemon possua dois tipos, e pelo menos um tipo foi previsto com sucesso, considera-se como um todo sucesso a previsão
+
+Foram usadas as bibliotecas do python <code>scikit-learn</code>, para criar a rede e tratar o conjunto de dados, <code>pandas</code>, para leitura do CSV e normatização, e <code>joblib</code>, para salvar em arquivo o conjunto de dados normatizado e a rede MLP
 
 ...
 
-A base de dados utilizada no algoritmo está disponível <a href="https://www.kaggle.com/datasets/alopez247/pokemon/code">neste link</a>, baixada como CSV
+A base de dados utilizada no algoritmo está disponível <a href="https://www.kaggle.com/datasets/alopez247/pokemon/code">neste link</a>, e foi baixada como CSV
--- main.py
+++ main.py
@@ -1,33 +1,44 @@
+# TODO: Remover alguns parâmetros desnecessários para avaliar o tipo
+# TODO: Mexer na função de classificação para melhorar acurácia
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier 
+from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score, classification_report
 import joblib
 
 dados = pd.read_csv('pokemon_alopez247.csv')
 
-X = dados.drop('Type_1', axis=1)
+X = dados.drop(['Type_1', 'Type_2', 'Name'], axis=1, errors='ignore')  # Remove colunas irrelevantes
 y = dados['Type_1']
 
+# Removendo nulos
+X = X.fillna(0)
 X = pd.get_dummies(X)
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=42, stratify=y
 )
 
-modelo = RandomForestClassifier(n_estimators=100, random_state=42)
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+modelo = MLPClassifier(
+    hidden_layer_sizes=(128, 64),  
+    activation='relu',             
+    solver='adam',                
+    max_iter=500,                
+    random_state=42
+)
 modelo.fit(X_train, y_train)
-
-
 y_pred = modelo.predict(X_test)
-print("Acurácia:", accuracy_score(y_test, y_pred))
-print(classification_report(y_test, y_pred))
 
+# O cálculo da acurácia considera acertar tanto o tipo primário quanto o tipo secundário, para pokemons que o possuem
 acertos_tipo1 = 0
 acertos_tipo1_ou_tipo2 = 0
 
-# Queremos verificar a acurácia tanto para acertar o tipo principal quanto para o tipo secundário
 for real_tipo1, real_tipo2, pred in zip(dados.loc[y_test.index, 'Type_1'], dados.loc[y_test.index, 'Type_2'], y_pred):
     if pred == real_tipo1:
         acertos_tipo1 += 1
@@ -39,6 +50,4 @@ acc_tipo1 = acertos_tipo1 / len(y_test)
 acc_tipo1_ou_tipo2 = acertos_tipo1_ou_tipo2 / len(y_test)
 
 print(f"Acurácia tipo primário exato: {acc_tipo1:.2%}")
-print(f"Acurácia tipo primário OU secundário: {acc_tipo1_ou_tipo2:.2%}")
-
-
+print(f"Acurácia tipo primário OU tipo secundário: {acc_tipo1_ou_tipo2:.2%}")
