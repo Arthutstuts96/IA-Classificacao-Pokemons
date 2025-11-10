159684d2f431c94a4104fa0e01799be01b5a16e8
Arthutstuts96 <oarthurlima1575@gmail.com>
2025-11-10 16:33:58 -0300

    wip: melhorando acurácia e balanceando classes

--- README.md
+++ README.md
@@ -2,8 +2,34 @@
 
 O objetivo desta atividade foi criar uma rede neural MLP capaz de resolver um problema de classificação definido, particularmente o de adivinhar o tipo de um dado pokemon a partir de alguns parâmetros coletados de uma base de dados. Caso o pokemon possua dois tipos, e pelo menos um tipo foi previsto com sucesso, considera-se como um todo sucesso a previsão
 
-Foram usadas as bibliotecas do python <code>scikit-learn</code>, para criar a rede e tratar o conjunto de dados, <code>pandas</code>, para leitura do CSV e normatização, e <code>joblib</code>, para salvar em arquivo o conjunto de dados normatizado e a rede MLP
+<h2>Ambiente</h2>
+
+Foram usadas as bibliotecas do python <code>scikit-learn</code>, para criar a rede e tratar o conjunto de dados, <code>pandas</code>, para leitura do CSV e normatização, <code>matplotlib</code>, para criar o gráfico da matriz de confusão, e <code>joblib</code>, para salvar em arquivo o conjunto de dados normatizado e a rede MLP
+
+A base de dados utilizada no algoritmo está disponível <a href="https://www.kaggle.com/datasets/alopez247/pokemon/code">neste link</a>, e foi baixada como CSV
+
+O código foi escrito no VSCode, e tudo foi executado no sistema operacional Linux. A versão do python utilizada é a <code>3.12.3</code>
+
+<h2>Primeiro teste</h2>
+
+Inicialmente, os parâmetros foram:
+
+```python
+modelo = MLPClassifier(
+    hidden_layer_sizes=(128, 64),  
+    activation='relu',             
+    solver='adam',                
+    max_iter=500,                
+    random_state=42
+)
+```
+
+Nesse molde, os resultados não foram satisfatórios, estão dispostos na imagem abaixo. Foram utilizadas as métricas de avaliação <i>precision, recall</i> e <i>f1-score</i>, contudo, para contabilizar sucessos no acerto de pelo menos um dos tipos (ambos serviriam como rótulos), foi criado um método diferente para cálculo da acurácia
+
+<img src="/imagens/primera_iteracao.png" alt="Imagem do primeiro teste" />
+
+A acurácia do primeiro tipo foi de 65.44%, enquanto que considerando ambos tipos foi 68.66%. O objetivo é obter uma acurácia de 90% para cima, de modo a considerar a rede satisfatória
 
 ...
 
-A base de dados utilizada no algoritmo está disponível <a href="https://www.kaggle.com/datasets/alopez247/pokemon/code">neste link</a>, e foi baixada como CSV
+Eventualmente, foi percebido o maior problema da rede: as classes estavam desbalanceadas, de modo que, a exemplo, o tipo voador "Flying", possuía somente dois registros, sendo ambos da mesma família Pokemon. Isso se deu pela característica dos jogos de Pokemon priorizarem o tipo voador como tipo secundário, o que fez com que o número de Pokemon dessa classe e outras caíssem tanto de modo que o algoritmo SMOTE também não foi capaz de suprir, pois criar dados sintéticos a partir de duas instâncias não gerou resultados desejáveis. Deste modo, foi necessária uma intervenção manual no conjunto de dados  
--- main.py
+++ main.py
@@ -1,39 +1,78 @@
-# TODO: Remover alguns parâmetros desnecessários para avaliar o tipo
-# TODO: Mexer na função de classificação para melhorar acurácia
+"""
+tirando alguns parâmetros
+diminui qnt de testes
+max_iter * 3
+batch_size menor
+RobustScaler
+matriz de confusão com matplotlib
+n_iter_no_change
+evitar normalizar valores binários transformados
+balancear classes com SMOTE
+"""
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import RobustScaler
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
+import matplotlib.pyplot as plt
+from imblearn.over_sampling import SMOTE
 import joblib
 
 dados = pd.read_csv('pokemon_alopez247.csv')
 
-X = dados.drop(['Type_1', 'Type_2', 'Name'], axis=1, errors='ignore')  # Remove colunas irrelevantes
+X = dados.drop(['Type_1', 'Type_2', 'Number', 'Pr_Male', 'Generation', 'hasGender', 'Name', 'hasMegaEvolution', 'isLegendary', 'Catch_Rate'], axis=1, errors='ignore')  # Remove colunas irrelevantes
 y = dados['Type_1']
 
 # Removendo nulos
 X = X.fillna(0)
 X = pd.get_dummies(X)
 
+scaler = RobustScaler()
+
+num_cols = ['HP', 'Attack', 'Defense', 'Sp_Atk', 'Sp_Def', 'Speed', 'Total', 'Height_m', 'Weight_kg']
+X_scaled = X.copy()
+X_scaled[num_cols] = scaler.fit_transform(X_scaled[num_cols])
+
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.3, random_state=42, stratify=y
+    X_scaled, y, test_size=0.2, random_state=42, stratify=y
 )
 
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+# Balanceando classes via SMOTE
+smote = SMOTE(random_state=42, k_neighbors=1)
+X_train_balanced, y_train_balanced = smote.fit_resample(X_train, y_train)
+print(f"Antes do SMOTE: {y_train.value_counts()}")
+print(f"Depois do SMOTE: {y_train_balanced.value_counts()}")
+
+#X_train = scaler.fit_transform(X_train)
+#X_test = scaler.transform(X_test)
 
 modelo = MLPClassifier(
-    hidden_layer_sizes=(128, 64),  
+    #hidden_layer_sizes=(128, 64),  
+    #hidden_layer_sizes=(256, 128),
+    hidden_layer_sizes=(512, 256, 128),
+
     activation='relu',             
-    solver='adam',                
-    max_iter=500,                
+    solver='adam',
+    batch_size=32,  
+
+    max_iter=2000,                
     random_state=42
 )
-modelo.fit(X_train, y_train)
+
+modelo.fit(X_train_balanced, y_train_balanced)
 y_pred = modelo.predict(X_test)
+
+# MATRIZ DE CONFUSÃO
+cm = confusion_matrix(y_test, y_pred, labels=modelo.classes_)
+
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=modelo.classes_)
+disp.plot(xticks_rotation=90, cmap='viridis')
+plt.title("Matriz de Confusão - Classificação de Pokémons")
+plt.savefig('resultados/matriz_confusao.png', dpi=300, bbox_inches='tight')
+
+print(classification_report(y_test, y_pred))
 
 # O cálculo da acurácia considera acertar tanto o tipo primário quanto o tipo secundário, para pokemons que o possuem
 acertos_tipo1 = 0
@@ -51,3 +90,26 @@ acc_tipo1_ou_tipo2 = acertos_tipo1_ou_tipo2 / len(y_test)
 
 print(f"Acurácia tipo primário exato: {acc_tipo1:.2%}")
 print(f"Acurácia tipo primário OU tipo secundário: {acc_tipo1_ou_tipo2:.2%}")
+
+'''
+y_counts = y_train.value_counts()
+X_train_res = X_train.copy()
+y_train_res = y_train.copy()
+
+for classe, count in y_counts.items():
+    if count < 79:
+        # Seleciona todos os exemplos dessa classe
+        X_class = X_train[y_train == classe]
+        y_class = y_train[y_train == classe]
+        
+        # Quantos registros faltam para atingir max_count
+        n_to_add = 79 - count
+        
+        # Amostra aleatória com reposição
+        X_oversample = X_class.sample(n=n_to_add, replace=True, random_state=42)
+        y_oversample = y_class.loc[X_oversample.index]
+        
+        # Adiciona ao conjunto de treino
+        X_train_res = pd.concat([X_train_res, X_oversample], axis=0)
+        y_train_res = pd.concat([y_train_res, y_oversample], axis=0)
+'''
